2b9e19e953639a6e5e095dac68672db898847cc3
samo79 <58866305+samo790@users.noreply.github.com>
2026-05-15 23:36:10 +0200

    Fixed stubs_68k.s

--- src/stubs_68k.s
+++ src/stubs_68k.s
@@ -1,4 +1,4 @@
-; ==============================================================================
+==============================================================================
 ; POWERUP/PPC.LIBRARY 68K STUB GENERATOR - PROJECT SYNC
 ; Version: 18.2
 ; ==============================================================================
@@ -11,7 +11,6 @@ MY_LIB_VER  EQU 18
 MY_LIB_REV  EQU 2
 
 ; --- TARGET SWITCH (0=PowerUP, 1=PPC) ---
-; Può essere impostato via riga di comando con -DTARGET_PPC=1
     IFND TARGET_PPC
 TARGET_PPC  EQU 0       
     ENDC
@@ -27,63 +26,62 @@ TARGET_PPC  EQU 0
     dc.b    105             ; lib_Flags (LIBF_SUMUSED|LIBF_CHANGED)
     dc.b    0               ; pad
     dc.w    34              ; lib_PosSize
-    dc.w    0               ; lib_NegSize (Verrà calcolato dall'assembler)
-    dc.w    MY_LIB_VER      ; Version 18
-    dc.w    MY_LIB_REV      ; Revision 2
+    dc.w    0               ; lib_NegSize (Calcolato dinamicamente)
+    dc.w    MY_LIB_VER      
+    dc.w    MY_LIB_REV      
     dc.l    idString        ; Puntatore alla stringa ID
     dc.l    0               ; lib_Sum
     dc.w    0               ; lib_OpenCnt
 
-    ; --- Macro Magic Trap con ID ---
+    ; --- Macro di aggancio corretto per i vettori ---
     macro PUP_STUB lvo, id
         dc.w    \lvo
-        dc.w    $4EF8, $0004
+        dc.w    $4EF8, $0004   ; Trappola d'ambiente per il bridge
         dc.l    \id
     endm
 
-    ; --- La Jump Table 68k ---
+    ; --- LA JUMP TABLE REALINEATA SUI VETTORI UFFICIALI PHASE5 ---
+    ; Nota: Gli offset devono corrispondere millimetricamente a ppc.library originale
     PUP_STUB -6,  100               ; Open
     PUP_STUB -12, 101               ; Close
     PUP_STUB -18, 102               ; Expunge
     PUP_STUB -24, 103               ; Reserved
-    PUP_STUB -30,  1                ; RunPPC_Master
-    PUP_STUB -54,  2                ; AllocSignalPPC
-    PUP_STUB -60,  3                ; FreeSignalPPC
-    PUP_STUB -90,  4                ; GetPPCState
-    PUP_STUB -300, 5                ; SetSignalPPC
-    PUP_STUB -306, 6                ; SPrintF_Real (VPrintf)
-    PUP_STUB -318, 7                ; AvailMem_Wrapper
-    PUP_STUB -324, 8                ; AllocVecPPC_Magic
-    PUP_STUB -330, 9                ; FreeVecPPC_Magic
-    PUP_STUB -336, 1                ; RunPPC (Re-entry)
-    PUP_STUB -378, 10               ; ObtainSemaphorePPC
-    PUP_STUB -390, 11               ; ReleaseSemaphorePPC
-    PUP_STUB -492, 12               ; WaitPPC
-    PUP_STUB -498, 13               ; NewList
-    PUP_STUB -594, 14               ; WaitTime_Wrapper
-    PUP_STUB -600, 15               ; GetInfo
-    PUP_STUB -606, 16               ; CreateMsgPortPPC
-    PUP_STUB -612, 17               ; DeleteMsgPortPPC
-    PUP_STUB -636, 18               ; PutMsgPPC
-    PUP_STUB -642, 19               ; GetMsgPPC
-    PUP_STUB -648, 20               ; ReplyMsgPPC
-    dc.w    0                       ; Fine tabella
+    
+    PUP_STUB -30,  1                ; PPCGetAttrsA
+    PUP_STUB -36,  2                ; PPCSetAttrsA
+    PUP_STUB -42,  3                ; PPCAllocMem
+    PUP_STUB -48,  4                ; PPCFreeMem
+    PUP_STUB -54,  5                ; PPCCreateTaskA (La chiamata chiave)
+    PUP_STUB -60,  6                ; PPCDeleteTask
+    PUP_STUB -66,  7                ; PPCWait
+    PUP_STUB -72,  8                ; PPCSignal
+    PUP_STUB -78,  9                ; PPCGetNextPPCMsg
+    PUP_STUB -84,  10               ; PPCPutPPCMsg
+    PUP_STUB -90,  11               ; PPCRepPPCMsg
+    PUP_STUB -96,  12               ; PPCAllocPPCMsg
+    PUP_STUB -102, 13               ; PPCFreePPCMsg
+    PUP_STUB -108, 14               ; PPCAllocSemaphore
+    PUP_STUB -114, 15               ; PPCFreeSemaphore
+    PUP_STUB -120, 16               ; PPCObtainSemaphore
+    PUP_STUB -126, 17               ; PPCAttemptSemaphore
+    PUP_STUB -132, 18               ; PPCReleaseSemaphore
+    PUP_STUB -138, 19               ; PPCFindTask
+    PUP_STUB -144, 20               ; PPCObtain
+    dc.w    0                       ; Fine tabella obbligatoria
 
-    ; --- Dati Identificativi (Standard AmigaOS) ---
+    ; --- Dati Identificativi ---
     IF TARGET_PPC=0
 libName:  dc.b "powerup.library",0
     ELSE
 libName:  dc.b "ppc.library",0
     ENDC
 
-    ; Nota: La stringa deve iniziare con $VER: per essere letta dal comando Version
     CNOP 0,2
 idString: 
     IF TARGET_PPC=0
-    dc.b "$VER: powerup.library 18.2 (10.05.2026) Gold",0
+    dc.b "$VER: powerup.library 18.2 (15.05.2026)",0
     ELSE
-    dc.b "$VER: ppc.library 18.2 (10.05.2026) Gold",0
+    dc.b "$VER: ppc.library 18.2 (15.05.2026)",0
     ENDC
 
     EVEN
-
